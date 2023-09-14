344066dab76df23c0809b12eecc84f6fbf49b3ac
ahosanhabib2121 <ahosanhabib21021@gmail.com>
2023-09-14 21:26:17 +0600

    Project basic setup and json fake data store in public folder

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      {/* header section */}
+      <div>
+        <h1 className='text-center my-12 header_title_style '>Course Registration</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
